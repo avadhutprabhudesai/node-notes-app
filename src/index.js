b6e20d4eb85af58703be73b5edeff5d88fa5a61b
Avadhut Prabhudesai <avadhutprabhudesai23@gmail.com>
2022-01-01 19:49:00 +0530

    feat: #Command line arguments
    Handling of command line arguments in node is illustrated using Yargs npm package and a small note
    taking app.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const getNotes = require('./notes');
-const chalk = require('chalk');
-console.log(getNotes());
-
-console.log(chalk.green.inverse('Success'));
-console.log(chalk.blue('This is a new message'));
-console.log(chalk.bgRed.yellow.bold('Avadhut'));
